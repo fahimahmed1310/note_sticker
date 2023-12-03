a58d53b49b64a8f569858092f76d21567a0e09fd
fahimahmed1310 <fahimahmed8798740@gmail.com>
2023-12-03 15:42:20 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    NoteSticker()
+  );
+}
+
+class NoteSticker extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
+
